164695a02b44df251bd70ab06d4556102f01f1c1
matt manes <mattmanes@pm.me>
2023-08-22 14:23:58 -0500

    Initial commit

---
+++ update_sqlitedb.py
@@ -0,0 +1,29 @@
+import pull_data
+import expobased
+from printbuddies import print_in_place
+from noiftimer import Timer
+from concurrent.futures import ProcessPoolExecutor
+import time
+
+
+def update():
+    pull_data.pull()
+    expobased.load_data_to_db()
+
+
+def main():
+    """Pull current datasets and load into database."""
+    timer = Timer().start()
+    with ProcessPoolExecutor() as executor:
+        process = executor.submit(update)
+        while not process.done():
+            print_in_place(
+                f"Updating sqlite database... {timer.elapsed_str}", animate=True
+            )
+            time.sleep(1)
+    print()
+    print("Update complete.")
+
+
+if __name__ == "__main__":
+    main()
