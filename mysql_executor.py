2a4f7f52e067ad9d8aa1811bc6e9e5008038c073
matt manes <mattmanes@pm.me>
2023-09-12 13:04:37 -0500

    refactor: put credential handling in separate functions

--- mysql_executor.py
+++ mysql_executor.py
@@ -1,3 +1,4 @@
+import getpass
 import os
 
 from noiftimer import time_it
@@ -6,7 +7,43 @@ from pathier import Pathier
 root = Pathier(__file__).parent
 
 
-def execute_mysql_script(sql_file: str):
+def load_creds() -> dict[str, str] | None:
+    """Read and return `creds.toml` if it exists, otherwise return `None`.
+
+    The mysql username should be put in a file called `creds.toml` under the field `username`.
+
+    To avoid manually typing in your password add a `password` field to `creds.toml` with the password.
+
+    The `.toml` file should look like (but with subbed values):
+    >>> username = <username>
+    >>> password = <password>
+
+    #### NOTE: This assumes an unimportant local mysql instance and the user accepts any security tradeoff for the convenience."""
+    file = root / "creds.toml"
+    if file.exists():
+        return file.loads()
+    return None
+
+
+def prompt_for_creds() -> dict[str, str]:
+    """Prompt the user for their username and password."""
+    creds = {}
+    creds["username"] = input("Enter MySQL username: ")
+    creds["password"] = getpass.getpass("Enter MySQL password: ")
+    return creds
+
+
+def get_creds() -> dict[str, str]:
+    """Try to load creds from `creds.toml`.
+
+    If it doesn't exist, prompt the user."""
+    creds = load_creds()
+    if not creds:
+        creds = prompt_for_creds()
+    return creds
+
+
+def execute_mysql_script(sql_file: str, creds: dict[str, str] | None):
     """Run the specified `sql_file`.
 
     This assumes that the folder containing `mysql.exe` is in your `PATH`.
@@ -15,32 +52,17 @@ def execute_mysql_script(sql_file: str):
 
     Mine is set to a value of `128M`.
 
-    The mysql server will need to be restarted for this to take effect.
-
-    The mysql username should be put in a file called `creds.toml` under the field `user`.
-
-    To avoid manually typing in your password add a `pass` field to `creds.toml` with the password.
-
-    The `.toml` file should look like (but with subbed values):
-    >>> user = "username"
-    >>> pass = "password"
-
-    #### NOTE: This assumes an unimportant local mysql instance and the user accepts any security tradeoff for the convenience."""
-    if not (root / "creds.toml").exists():
-        creds = {}
-        creds["user"] = input("Enter your mysql instance username: ")
-    else:
-        creds: dict = (root / "creds.toml").loads()
-    if creds.get("pass"):
-        os.system(f"mysql -u{creds['user']} -p{creds['pass']} < {sql_file}")
-    else:
-        os.system(f"mysql -u{creds['user']} -p < {sql_file}")
+    The mysql server will need to be restarted for this to take effect."""
+    if not creds:
+        creds = get_creds()
+    os.system(f"mysql -u{creds['username']} -p{creds['password']} < {sql_file}")
 
 
 @time_it()
 def main():
+    creds = get_creds()
     for file in ["chidata_ddl_mysql.sql", "chidata_dml_mysql.sql", "mysql_views.sql"]:
-        execute_mysql_script(file)
+        execute_mysql_script(file, creds)
 
 
 if __name__ == "__main__":
